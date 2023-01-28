67a6fc01ccb5458197fa3d0c8be6f793c6e8a340
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-28 19:28:42 +0600

    array-loop file add

---
+++ array-loop.js
@@ -0,0 +1,6 @@
+var numbers = [45, 87, 89, 56, 32, 51, 25, 32, 58]
+
+for(i = 0 ; i <= numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
---
+++ for.js
@@ -0,0 +1,14 @@
+// var mango = 0 ;
+// while(mango < 10){
+//     console.log(mango);
+//     mango++;
+// }
+
+// for(var mango = 0 ; mango <= 10 ; mango++){
+//     console.log('mango khamu 10 ta');
+//     console.log(mango);
+// }
+// simple version
+for(var i = 0 ; i <= 10 ; i++){
+    console.log(i);
+}
