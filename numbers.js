9752ffcb49c6421ee4f8edf79b30679d485f4ff3
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-28 19:00:28 +0600

    numbers file add

---
+++ numbers.js
@@ -0,0 +1,11 @@
+// var numbers = 0;
+// while(numbers <= 10){
+//     console.log(numbers);
+//     numbers++;
+// }
+
+var numbers = 0;
+while(numbers <= 100){
+    console.log(numbers);
+    numbers +=2;
+}
