d9c4de317b85afbe4fc667575ac6238e8d2f6f10
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-28 18:41:41 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,12 @@
+var rice = 0 ;
+while( rice <= 7 ){
+    console.log('amake taratari daw')
+    console.log(rice);
+    rice++;
+}
+// var rostGiven = 0;
+// while( rostGiven < 7){
+//     console.log('Rost den please, !!');
+//     console.log(rostGiven);
+//     rostGiven++;
+// }
