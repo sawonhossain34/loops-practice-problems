193b8f780138de4a37f6aa4ea575c400eab9060b
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-29 12:10:05 +0600

    variable file add

---
+++ variable.js
@@ -0,0 +1,42 @@
+// !!!!!variable change
+
+// var numbers =[ 25, 54, 78,];
+// numbers[2] = 80;
+// console.log(numbers);
+
+//        index of number cheack
+// var numbers = [25, 36, 69,78];
+// var number = numbers[2];
+// console.log(number);
+
+    //    type of number string boolean
+// var numbers = [true];
+// var number =(typeof numbers[0]);
+// console.log(number);
+
+// variable tofixed !!!!!
+// var numbers = 3.568 ;
+// var number = numbers.toFixed(2);
+// console.log(number);
+
+// parseFloat 
+// var numbers = '35.45757' ;
+// // var number = numbers.parseFloat;
+// console.log(parseFloat(numbers));
+
+
+// array 
+// var numbers = [1, 3, 4, 5, 6, 7, 9, 15, 58];
+// length ber korte .length dite hobe
+// var number = numbers.length;
+// console.log(number);
+
+// var numbers = [1, 3, 4, 5, 6, 7, 9, 15, 58];
+// numbers[2] = 6;
+// console.log(numbers);
+
+
+// array element number chack
+// var numbers = [1, 3, 4, 5, 6, 7, 9, 15, 58]
+// var number = numbers.indexOf(15);
+// console.log(number);
